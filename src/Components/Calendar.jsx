cac8a6ff37a6435b903355e35c997d93806e514c
PivrikArtem <artempivrik90@gmail.com>
2019-02-12 23:45:58 +0200

    add new Componens

---
+++ src/Components/Calendar.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import s from '../../src/App.module.css';
+
+class Calendar extends Component{
+render(){
+    return(
+        <div className={s.wrapperCalendar}>
+            Cool App;
+        </div>
+    )
+}
+}
+export default Calendar;
---
+++ src/Components/Navbav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from '../../src/App.module.css';
+const Navbar = () => {
+    return (
+        <nav className={s.navbar}>
+            <span>Frontend Developer Conference in 2019</span>
+        </nav>
+    );
+}
+
+
+export default Navbar;
