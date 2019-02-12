20ff12b239ed03d3a8288c88656cce1eacf7b764
PivrikArtem <artempivrik90@gmail.com>
2019-02-12 23:47:19 +0200

    add css module and changing in root App file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import icon from './icon.svg';
+import Navbar from './Components/Navbav';
+import Calendar from './Components/Calendar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Calendar/>
+    </>
+  );
 }
+
 
 export default App;
