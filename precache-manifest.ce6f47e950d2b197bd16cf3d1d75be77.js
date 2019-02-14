ec72e0f1972c26d58df4ea469630c71682e32247
PivrikArtem <artempivrik90@gmail.com>
2019-02-14 02:27:01 +0200

    Updates

---
+++ precache-manifest.ce6f47e950d2b197bd16cf3d1d75be77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba492e131d8ad91f2691",
+    "url": "/calendar-of-events/static/css/main.d263a248.chunk.css"
+  },
+  {
+    "revision": "ba492e131d8ad91f2691",
+    "url": "/calendar-of-events/static/js/main.ba492e13.chunk.js"
+  },
+  {
+    "revision": "cb6690657d5027add252",
+    "url": "/calendar-of-events/static/js/runtime~main.cb669065.js"
+  },
+  {
+    "revision": "70b62554493970a38f8b",
+    "url": "/calendar-of-events/static/js/2.70b62554.chunk.js"
+  },
+  {
+    "revision": "754efd14ba268f1467f1cd770270023d",
+    "url": "/calendar-of-events/index.html"
+  }
+];
